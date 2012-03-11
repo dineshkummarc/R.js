f15695923392f727342ecfb2dfc6b052704bea11
Neil McCallion <neil@artistx06.(none)>
2012-03-11 17:31:53 +0000

    Added 'map' as an instance method

--- R.js
+++ R.js
@@ -92,6 +92,7 @@
 	}
 
 	// basic array iterator (didn't call it 'each' to discourage rampant abuse)
+	// callback 'f' should have args (item, <opt>index)
 	R.map = function(arr, f) {
 		if(!arr instanceof Array) throw('R.map(): arg 1 must be an array');
 		if(typeof f != 'function') throw('R.map(): arg 2 must be a function');
@@ -121,6 +122,12 @@
 				_setStyle(key, obj[key]);
 			}
 			return _; // Chainable
+		},
+
+		// instance-specific variant of R.map that uses the instance collection as an array
+		// # TODO: is this an OK way to approach this?
+		map: function(f) {
+			R.map(_.collection, f);
 		},
 
 		// is this Numeric?
